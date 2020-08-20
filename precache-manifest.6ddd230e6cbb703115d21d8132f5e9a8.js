6497ad2a12b5c05a6ce6b0b0fcee3c153c287fac
Kakha1998 <kakha.gujejiani.1@iliauni.edu.ge>
2020-08-20 10:17:00 +0400

    Updates

---
+++ precache-manifest.6ddd230e6cbb703115d21d8132f5e9a8.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "89d5292aad7893c3f6d54bab3bbce41a",
+    "url": "/drum-machine/index.html"
+  },
+  {
+    "revision": "5c47bc1b5826a4eab901",
+    "url": "/drum-machine/static/css/main.c33ead47.chunk.css"
+  },
+  {
+    "revision": "0cd9f3258285c15cba0b",
+    "url": "/drum-machine/static/js/2.ee155c99.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/drum-machine/static/js/2.ee155c99.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5c47bc1b5826a4eab901",
+    "url": "/drum-machine/static/js/main.7a75d17e.chunk.js"
+  },
+  {
+    "revision": "d7d1943964fcc80e9287",
+    "url": "/drum-machine/static/js/runtime-main.49a0ba3c.js"
+  }
+]);
